9e79bc3c031da527468bb841dff0a0844e6da7c5
Rob Ruenes <rob@ruenes.dev>
2023-10-21 23:06:56 -0400

    Finish through strings.

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -7,17 +7,19 @@
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut s = String::from(input);
+    s.push_str(" world!");
+    s
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    String::from(input.replace("cars", "balloons"))
 }
 
 #[cfg(test)]
